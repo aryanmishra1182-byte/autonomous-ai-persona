152a28544300f690d9e44ac4a332428d5f3e5153
aryan mishra <aryanmishra1182@gmail.com>
2026-08-07 23:19:27 +0530

    docs(api): add OpenAPI specification config metadata

---
+++ src/main/java/com/persona/config/OpenApiConfig.java
@@ -0,0 +1,11 @@
+package com.persona.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI & API Documentation specifications for Autonomous Persona API
+ */
+@Configuration
+public class OpenApiConfig {
+    // API Documentation Metadata Holder
+}
